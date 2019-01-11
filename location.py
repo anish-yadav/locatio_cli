18dfc6bc84a4d4b2249760dbaa653da60c194f88
Anish Yadav <hney1000.ay1@gmail.com>
2019-01-11 17:16:14 +0530

    Added if else case

--- location.py
+++ location.py
@@ -4,14 +4,20 @@ import requests
 def get_location():
     url = "http://ipinfo.io"
     response = requests.get(url)
-    location = response.json()['loc']
-    return location
+    if response.status_code == 200:
+        location = response.json()['loc']
+        return location
+    else:
+        return " unable to get the location"
 
 def get_address(latitude,longitude):
     url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat="+latitude+"&lon="+longitude
     response = requests.get(url)
-    address = response.json()["display_name"]
-    return "Your address is  : {}".format(address)
+    if response.status_code == 200:
+         address = response.json()["display_name"]
+         return "Your address is  : {}".format(address)
+    else:
+       return "Unable to get the Address"
 
 
 if len(sys.argv) <2 :
